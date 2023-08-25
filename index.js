863963ed38fc472585a5191536f9386ab8275f52
Brian Richie Sr <brianrichiesr@gmail.com>
2023-08-24 23:00:56 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(arr, greeting) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${greeting} gift!`);
+    }
+
+    return newArr;
+
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(num);
+        num--;
+    }
+}
